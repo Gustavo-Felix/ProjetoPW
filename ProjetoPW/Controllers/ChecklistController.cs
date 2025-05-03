b2aa25bbc1e841517aa7b16abea9cd7b6c8f0294
Administrator <Administrator@WIN-U7SINVC8IIT>
2025-05-03 12:30:05 -0300

    Base feita, falta aprimorar e mudar variaveis

---
+++ ProjetoPW/Controllers/ChecklistController.cs
@@ -0,0 +1,72 @@
+﻿using ProjetoPW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoPW.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        // GET: Checklist
+        public ActionResult Index() => RedirectToAction("Listar");
+
+        // Listar todos os Checklists
+        public ActionResult Listar()
+        {
+            Checklist.GerarLista(Session);
+            return View(Session["ListaChecklist"] as List<Checklist>);
+        }
+
+        // Criar um novo Checklist
+        public ActionResult Create() => View(new Checklist());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Checklist Checklist)
+        {
+            if (ModelState.IsValid)
+            {
+                Checklist.Adicionar(Session);
+                return RedirectToAction("Listar");
+            }
+            return View(Checklist);
+        }
+
+        // Editar um Checklist
+        public ActionResult Edit(int id)
+        {
+            var checklist = Checklist.Procurar(Session, id);
+            if (checklist == null)
+                return HttpNotFound();
+
+            return View(checklist);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Checklist Checklist)
+        {
+            if (ModelState.IsValid)
+            {
+                Checklist.Editar(Session, id);
+                return RedirectToAction("Listar");
+            }
+            return View(Checklist);
+        }
+
+        // Exclusão de Checklist com Ajax
+        [HttpPost]
+        public ActionResult DeleteAjax(int id)
+        {
+            var checklist = Checklist.Procurar(Session, id);
+            if (checklist != null)
+            {
+                checklist.Excluir(Session);
+                return Json(new { sucesso = true });
+            }
+            return new HttpStatusCodeResult(404, "Checklist não encontrado");
+        }
+    }
+}
---
+++ ProjetoPW/Models/Checklist.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoPW.Models
+{
+    public class Checklist
+    {
+        public int id { get; set; }
+        public string nomeTarefa { get; set; }
+        [Required(ErrorMessage = "É necessário dar um nome a tarefa!")]
+        public string descricao { get; set; }
+        [Required(ErrorMessage = "É necessário colocar uma descrição!")]
+        public bool concluido { get; set; }
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime dataCadastro { get; set; }
+
+        public static void GerarLista(HttpSessionStateBase session)
+        {
+            if (session["ListaChecklist"] != null && ((List<Checklist>)session["ListaChecklist"]).Count > 0)
+            {
+                return;
+            }
+            var lista = new List<Checklist>
+            {
+                new Checklist { id = 0, nomeTarefa = "Teste", descricao = "Esta é uma tarefa exemplo!", concluido = false, dataCadastro = new DateTime(2025, 01, 01) },
+            };
+            session["ListaChecklist"] = lista;
+        }
+        public void Adicionar(HttpSessionStateBase session)
+        {
+            var lista = session["ListaChecklist"] as List<Checklist>;
+            if (lista == null)
+            {
+                lista = new List<Checklist>();
+                session["ListaChecklist"] = lista;
+            }
+
+            this.id = lista.Count > 0 ? lista.Max(a => a.id) + 1 : 0;
+            lista.Add(this);
+        }
+
+        public static Checklist Procurar(HttpSessionStateBase session, int id)
+        {
+            var lista = session["ListaChecklist"] as List<Checklist>;
+            return lista?.FirstOrDefault(a => a.id == id);
+        }
+
+        public void Editar(HttpSessionStateBase session, int id)
+        {
+            var lista = session["ListaChecklist"] as List<Checklist>;
+            var original = lista?.FirstOrDefault(a => a.id == id);
+
+            if (original != null)
+            {
+                original.nomeTarefa = this.nomeTarefa;
+                original.descricao = this.descricao;
+                original.concluido = this.concluido;
+                original.dataCadastro = this.dataCadastro;
+            }
+        }
+
+        public void Excluir(HttpSessionStateBase session)
+        {
+            var lista = session["ListaChecklist"] as List<Checklist>;
+            lista?.RemoveAll(a => a.id == this.id);
+        }
+    }
+}
